0b81dda5b63048e40be1ab43f6206eecef196831
waheed-glumatic <waheed-glumatic@outlook.com>
2021-04-12 03:30:12 +1000

    added a test skeleton

---
+++ tests/PremiumCalculator.Api.Tests/Services/PremiumServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using PremiumCalculator.Api.Services;
+using NUnit.Framework;
+
+namespace PremiumCalculator.Api.Services.Tests
+{
+    [TestFixture()]
+    public class PremiumServiceTests
+    {
+        [Test()]
+        public void CalculateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
