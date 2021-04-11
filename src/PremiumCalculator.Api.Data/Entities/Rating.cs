d15916394476502492b833be4ed3c60ca56f36a8
waheed-glumatic <waheed-glumatic@outlook.com>
2021-04-12 02:24:28 +1000

    Removed id from Rating entity

--- src/PremiumCalculator.Api.Data/Entities/Rating.cs
+++ src/PremiumCalculator.Api.Data/Entities/Rating.cs
@@ -5,7 +5,6 @@ namespace PremiumCalculator.Api.Data.Entities
 {
     public class Rating : BaseEntity
     {
-        public int Id { get; set; }
         public string Label { get; set; }
         public float Factor { get; set; }
         public virtual List<Occupation> Occupations { get; set; }
